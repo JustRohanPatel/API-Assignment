60e91b5b61f343a14e29474603e443a1baea3274
rohan patel <rpatel5@bsd220.org>
2025-03-04 09:23:23 -0600

    Initial Commit

---
+++ API Assignment/API_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_AssignmentApp.swift
+//  API Assignment
+//
+//  Created by Rohan Patel on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct API_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API AssignmentTests/API_AssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  API_AssignmentTests.swift
+//  API AssignmentTests
+//
+//  Created by Rohan Patel on 3/4/25.
+//
+
+import Testing
+@testable import API_Assignment
+
+struct API_AssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
